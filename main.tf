812fc348b3b6ea31c5697c05604e6b847fd839ee
vishnureddy <tarigopula34@gmail.com>
2025-01-03 16:40:20 +0530

    ansible

--- main.tf
+++
@@ -1,8 +0,0 @@
-module "ec2" {
-  for_each = var.instances
-  source = "./modules/ec2"
-  env = "var.env"
-  app_port = each.value["app_port"]
-  component_name = each.key
-  instance_type = each.value["instance_type"]
-}
